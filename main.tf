592d9acf3587433c8b05aca6b8921a4d7983937d
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-03 15:08:34 +0530

    v4

--- main.tf
+++ main.tf
@@ -16,19 +16,19 @@ resource "aws_instance" "ec2_example_01" {
   }
 
     # Root block device
-  root_block_device {
-    volume_type           = "gp2"            # General Purpose SSD (covered under free tier)
-    volume_size           = 10                # Size in GB (within free tier limits)
-    delete_on_termination = true             # Delete the volume on instance termination
-  }
-
-  # Additional EBS data disk (optional)
-  # ebs_block_device {
-  #   device_name           = "/dev/sdh"       # Device name for the additional volume
+  # root_block_device {
   #   volume_type           = "gp2"            # General Purpose SSD (covered under free tier)
-  #   volume_size           = 10               # Size in GB (within free tier limits for a total of 28 GB)
+  #   volume_size           = 10                # Size in GB (within free tier limits)
   #   delete_on_termination = true             # Delete the volume on instance termination
   # }
+
+  # Additional EBS data disk (optional)
+  ebs_block_device {
+    device_name           = "/dev/sdh"       # Device name for the additional volume
+    volume_type           = "gp2"            # General Purpose SSD (covered under free tier)
+    volume_size           = 10               # Size in GB (within free tier limits for a total of 28 GB)
+    delete_on_termination = true             # Delete the volume on instance termination
+  }
 
   lifecycle {
     ignore_changes = [ tags ]
