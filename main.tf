41d7034b8ffcf6b84011cff4ce352205e9e2ef62
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-03 14:44:06 +0530

    modify

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region     = "us-east-1" 
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+
 resource "aws_instance" "ec2_example" {
   ami           = var.ami_id
   instance_type = var.instance_type
